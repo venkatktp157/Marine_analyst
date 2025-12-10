ef0a0efddd8ec9ea528f31642642f38e68d785a7
venkatktp157 <vatsal157@gmail.com>
2025-12-10 10:57:16 +0530

    modified llm

--- mar.py
+++ mar.py
@@ -17,7 +17,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 # Initialize Groq LLM using Streamlit secrets
 # ---------------------------
 llm = ChatGroq(
-    model="llama-3.3-70b-versatile",
+    model="llama-3.1-8b-instant",
     temperature=0,
     groq_api_key=st.secrets["GROQ_API_KEY"]
 )
